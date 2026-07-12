a313ac347214e8593117bd9dd362e828e00161fe
HuntOrBeHunted83 <learn.ishank.ramnath@gmail.com>
2026-07-12 15:35:30 -0500

    Musics Playing On Tab Activation

--- background.js
+++ background.js
@@ -1,3 +1,51 @@
+let creatingOffscreen;
+
+async function createOffScreenDoc (url) {
+  const offscreenUrl = chrome.runtime.getURL('offscreen.html');
+  let offscreenDocExists = false;
+
+  if ('getContexts' in chrome.runtime) {
+    const contexts = await chrome.runtime.getContexts({
+      contextTypes: ['OFFSCREEN_DOCUMENT'],
+      documentUrls: [offscreenUrl],
+    });
+
+    if (contexts.length > 0) {
+      offscreenDocExists = true;
+      console.log('Offscreen Doc already created');
+    }
+  } else {
+    const clients = await self.clients.matchAll();
+    offscreenDocExists = clients.some((client) => client.url === offscreenUrl);
+  }
+
+  if (!offscreenDocExists) {
+    if (!creatingOffscreen) {
+      creatingOffscreen = chrome.offscreen.createDocument({
+        url: 'offscreen.html',
+        reasons: ['AUDIO_PLAYBACK'],
+        justification: "",
+      });
+
+      await creatingOffscreen;
+      creatingOffscreen = null;
+      console.log('Offscreen Document Made');
+    } else {
+      await creatingOffscreen;
+    }
+  }
+
+  chrome.runtime.sendMessage({ type: 'PLAY_AUDIO', url });
+}
+
+
+chrome.runtime.onStartup.addListener(() => {
+  createOffScreenDoc(chrome.runtime.getURL('chime.mp3'));
+  console.log('Extension started, offscreen document ensured');
+});
+
+
+
 
 chrome.tabs.onActivated.addListener(async (activeInfo) => {
   const tab = await chrome.tabs.get(activeInfo.tabId);
@@ -10,10 +58,10 @@ chrome.tabs.onActivated.addListener(async (activeInfo) => {
   try {
     let pageText = await chrome.tabs.sendMessage(activeInfo.tabId, { type: "PAGE_TEXT" });
     console.log("Page Text Received: ", pageText.text);
+    setMood(pageText.text)
+
+    createOffScreenDoc(chrome.runtime.getURL('songs/believer.mp3'));
   } catch (err) {
     console.log("Could not reach content script:", err.message);
   }
 });
-
-
-
---
+++ offscreen.js
@@ -0,0 +1,32 @@
+let player = null
+
+function playAudio(url) {
+    if (!player) {
+        player = new Audio();
+        player.addEventListener('error', (e) => {
+        console.error('Audio playback failed:', e);
+        });
+    }
+    player.src = url;
+    player.currentTime = 0;
+    player.loop = true;   
+    player.play().catch((err) => console.error('play() rejected:', err));
+}
+
+function stopAudio() {
+    if (player) {
+        player.pause();
+        player.currentTime = 0;
+    }
+}
+
+
+
+
+chrome.runtime.onMessage.addListener((message) => {
+    if (message.type === 'PLAY_AUDIO') {
+    playAudio(message.url);
+    } else if (message.type === 'STOP_AUDIO') {
+    stopAudio();
+    }
+});
